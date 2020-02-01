71b1ca832ee0728e3e4a48dbfec0007eb6dd411e
Vinicius Hashirama <vinicius_hashirama@outlook.com>
2020-02-01 11:44:22 -0300

    First Project Commit

---
+++ Assets/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+  public int currentScore;
+
+  public int recordScore;
+
+
+  
+}
+public enum GameState 
+{
+    MENU,
+    PREPARING,
+    INGAME,
+    FINALSCREEN
+}
---
+++ Assets/GameEventSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEventSystem : MonoBehaviour
+{
+        public delegate void GameEvent();
+        public delegate void GameEventPassIntValue(int value);
+        public delegate float GameFloatValue();
+        public delegate int GameIntValue();
+        public delegate bool GameBoolValue();
+
+
+        public static event GameEvent onGameStart;
+        public static void StartGame()
+        {
+            if (onGameStart != null)
+                onGameStart();
+        }
+
+        public static event GameEvent onGameFinish;
+        public static void FinishGame()
+        {
+            if (onGameFinish != null)
+                onGameFinish();
+        }
+
+        public static event GameEvent onGameReset;
+        public static void ResetGame()
+        {
+            if (onGameReset != null)
+                onGameReset();
+        } 
+}
